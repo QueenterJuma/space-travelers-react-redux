271f9a9bb959b3812b7a8e1d1a6664eb1eb7273d
edubew <edubew98@gmail.com>
2023-04-11 04:12:18 +0300

    Add basic styling to the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Navbar = () => (
-  <header>
+  <header className="header-container">
     <Link className="brand" to="/">
       <img src={logo} alt="logo" />
       <h2>Space Travelers Hub</h2>
     </Link>
-    <ul>
+    <ul className="nav-container">
       <li className="nav-item"><Link to="/">Rockets</Link></li>
       <li className="nav-item"><Link to="missions">Missions</Link></li>
       <li className="nav-item"><Link to="profile">My Profile</Link></li>
