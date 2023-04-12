8f9c62e47a6da7702b1e9ba66cc2abb56b9115fa
QueenterJuma <jumahqueenter@gmail.com>
2023-04-12 08:00:49 +0300

    Add reduxtoolkit

---
+++ src/Redux/Mission/missionSlice.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const url = 'https://api.spacexdata.com/v3/missions';
+const initialState = {
+  missions: [],
+  isLoading: false,
+};
+
+export const fetchMissions = createAsyncThunk(
+  'missions/FETCHMISSION',
+  async () => {
+    const response = await axios.get(url);
+    const newArr = await response.data.map((mission) => ({
+      id: mission.mission_id,
+      name: mission.mission_name,
+      description: mission.description,
+    }));
+    return newArr;
+  },
+);
+
+const missionSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: {
+    joinMission: (state, action) => {
+      const mission = state.missions.find((mision) => mision.id === action.payload);
+      if (mission) {
+        mission.reserved = !mission.reserved;
+      }
+    },
+    leaveMission: (state, action) => {
+      const mission = state.missions.find(
+        (mision) => mision.id === action.payload,
+      );
+      if (mission) {
+        mission.reserved = !mission.reserved;
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchMissions.pending, (state) => {
+      const isLoading = true;
+      return {
+        ...state,
+        isLoading,
+      };
+    });
+    builder.addCase(fetchMissions.fulfilled, (state, action) => {
+      const isLoading = false;
+      const missions = action.payload;
+
+      return {
+        ...state,
+        missions,
+        isLoading,
+      };
+    });
+  },
+});
+
+export const { joinMission, leaveMission } = missionSlice.actions;
+export default missionSlice.reducer;
