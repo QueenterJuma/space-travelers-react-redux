e4d133dc6ac0098550d42c66f3cd5a9971e03f57
edubew <edubew98@gmail.com>
2023-04-12 06:19:31 +0300

    Fix eslint errors

--- src/Components/Rocket.js
+++ src/Components/Rocket.js
@@ -1,5 +1,5 @@
-import React from "react";
-import logo from "../assets/logo.png";
+import React from 'react';
+import logo from '../assets/logo.png';
 
 const Rocket = () => (
   <div className="rockets-container">
