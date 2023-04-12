abce558e57e6937ea1ebdadf9fed3b5acd88b6a1
edubew <edubew98@gmail.com>
2023-04-12 07:30:01 +0300

    Create action creators for the rocketsSlice

--- src/Redux/rockets/rocketsSlice.js
+++ src/Redux/rockets/rocketsSlice.js
@@ -8,10 +8,17 @@ const initialState = [];
 
 // Add a reducer
 export default function rocketsReducer(state = initialState, action) {
-    switch (action.type) {
-        case FETCH_ROCKETS:
-            return action.payload;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case FETCH_ROCKETS:
+      return action.payload;
+    default:
+      return state;
+  }
 }
+
+// Action creator
+export const fetchRockets = createAsyncThunk(FETCH_ROCKETS, async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+});
