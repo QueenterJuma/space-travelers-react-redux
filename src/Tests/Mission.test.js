3a55ddbdff356e55c8e57667a09a16f20e7d78c2
QueenterJuma <jumahqueenter@gmail.com>
2023-04-14 17:33:23 +0300

    Add test to navbar

---
+++ src/Tests/Mission.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../Redux/store';
+import Mission from '../Components/Missions';
+
+it('renders Mission correctly', () => {
+  const tree = renderer
+    .create(
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Mission />
+          </Router>
+        </Provider>
+      </React.StrictMode>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
