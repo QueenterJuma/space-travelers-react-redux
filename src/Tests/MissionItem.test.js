92c7798ac7b9789f774b988fb0d2938b700ed4a9
QueenterJuma <jumahqueenter@gmail.com>
2023-04-14 17:33:59 +0300

    Add test to MissionItem

--- src/Tests/MissionItem.test.js
+++ src/Tests/MissionItem.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import rendered from 'react-test-renderer';
+import render from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { render, screen, fireEvent } from '@testing-library/react';
 import configureMockStore from 'redux-mock-store';
-import store from '../redux/store';
+import store from '../Redux/store';
 import Missionitem from '../components/MissionItem';
 
 it('renders MissionItem correctly', () => {
@@ -26,11 +26,23 @@ test('join a mission', () => {
     missions: {
       missions: [
         {
-          id: '2'
-          name: 'Telstar'
-          description: 'Telstar 19V (Telstar 19 Vantage) is a communication'
-        }
-      ]
-    }
-  }
-})
+          id: '2',
+          name: 'Telstar',
+          description: 'Telstar 19V (Telstar 19 Vantage) is a communication',
+        },
+      ],
+      isLoading: false,
+    },
+  };
+  const store = mockStore(initialState);
+  store.clearActions();
+  render(
+    <Provider>
+      <Missionitem />
+    </Provider>,
+  );
+  const joinButton = screen.getByRole('button', { name: /Join Mission/i });
+  fireEvent.click(joinButton);
+
+  expect(store.getActions()).toEqual([{ type: 'missions/joinMission' }]);
+});
