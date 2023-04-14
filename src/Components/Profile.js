79ea63250f262f6ee0bdc6d7aa4c8ffff83e32de
QueenterJuma <jumahqueenter@gmail.com>
2023-04-14 06:27:14 +0300

    Add tests files

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import MissionProfile from './missionProfile';
+import { useSelector } from 'react-redux';
+import '../style/Mission.css';
 
-const Profile = () => (
-  <div>
-    <MissionProfile />
-    <h1>Rocket</h1>
-  </div>
-);
+const Profile = () => {
+  const { missions } = useSelector((state) => state.missions);
+  // const { rockets } = useSelector((state) => state.rockets);
+  return (
+    <div>
+      <div className="mission-list">
+        <h2>My Mission</h2>
+        <table className="profile-table">
+          <tbody>
+            {missions
+              .filter((mission) => mission.join)
+              .map((join) => (
+                <tr key={join.id}>
+                  <td>{join.name}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+      {/* <div className="rocket-list">
+        <h2>My Rockets</h2>
+        <table className="profile-table">
+          <tbody>
+            {rockets
+              .filter((rocket) => rocket.reserved)
+              .map((reserved) => (
+                <tr key={reserved.id}>
+                  <td>{reserved.name}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div> */}
+    </div>
+  );
+};
 
 export default Profile;
