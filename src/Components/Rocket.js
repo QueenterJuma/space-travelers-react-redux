0680de0ac490ddd1b6d1d12267ec6ff953c99e20
edubew <edubew98@gmail.com>
2023-04-15 11:17:26 +0300

    Implement the suggested changes

--- src/Components/Rocket.js
+++ src/Components/Rocket.js
@@ -3,28 +3,26 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { makeReservation, cancelReservation } from '../Redux/rockets/rocketsSlice';
 
-const Rocket = ({
-  name, description, image, id, reserved,
-}) => {
+const Rocket = ({ rocket }) => {
   const dispatch = useDispatch();
 
   return (
     <div className="rocket-container">
-      <img src={image} alt="rocket" />
+      <img src={rocket.flickr_image} alt="rocket" />
       <div className="content-container">
-        <h2>{name}</h2>
+        <h2>{rocket.name}</h2>
         <p>
-          {reserved && <span className="reservation">Reserved</span>}
-          {description}
+          {rocket.reserved && <span className="reservation">Reserved</span>}
+          {rocket.description}
         </p>
-        {reserved ? (
-          <button className="cancel-button" type="submit" onClick={() => { dispatch(cancelReservation(id)); }}>Cancel Reservation</button>
+        {rocket.reserved ? (
+          <button className="cancel-button" type="submit" onClick={() => { dispatch(cancelReservation(rocket.id)); }}>Cancel Reservation</button>
         ) : (
           <button
             type="submit"
             className="reserve-button"
             onClick={() => {
-              dispatch(makeReservation(id));
+              dispatch(makeReservation(rocket.id));
             }}
           >
             Reserve Rocket
@@ -37,11 +35,13 @@ const Rocket = ({
 };
 
 Rocket.propTypes = {
-  name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
-  reserved: PropTypes.bool.isRequired,
+  rocket: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    flickr_image: PropTypes.string,
+    reserved: PropTypes.bool,
+  }).isRequired,
 };
 
 export default Rocket;
--- src/Redux/rockets/rocketsSlice.js
+++ src/Redux/rockets/rocketsSlice.js
@@ -2,54 +2,76 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const url = 'https://api.spacexdata.com/v4/rockets';
 
-const FETCH_ROCKETS = 'space-travelers-react-redux/rockets/fetchRockets';
-
-// Action creator
-export const fetchRockets = createAsyncThunk(FETCH_ROCKETS, async () => {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-});
-
 const initialState = {
   rockets: [],
-  reserved: false,
+  isLoading: true,
 };
+export const fetchedRockets = createAsyncThunk(
+  'rockets/fetchedRockets',
+  async () => {
+    try {
+      const res = await fetch(url);
+      const data = res.json();
+      return data;
+    } catch (error) {
+      return error;
+    }
+  },
+);
 
-// Add a reducer
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {
     makeReservation: (state, action) => {
-      const newState = state.rockets.map((rocket) => {
+      const newArr = state.rockets.map((rocket) => {
         if (rocket.id !== action.payload) return rocket;
         return { ...rocket, reserved: true };
       });
       return {
         ...state,
-        rockets: newState,
+        rockets: newArr,
       };
     },
     cancelReservation: (state, action) => {
-      const newState = state.rockets.map((rocket) => {
+      const newArr = state.rockets.map((rocket) => {
         if (rocket.id !== action.payload) return rocket;
         return { ...rocket, reserved: false };
       });
       return {
         ...state,
-        rockets: newState,
+        rockets: newArr,
       };
     },
   },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchRockets.fulfilled, (state, action) => ({
+      .addCase(fetchedRockets.pending, (state) => ({
         ...state,
-        rockets: action.payload,
+        isLoading: true,
+      }))
+      .addCase(fetchedRockets.fulfilled, (state, action) => {
+        const newArr = [];
+        action.payload.map((item) => newArr.push({
+          id: item.id,
+          name: item.name,
+          description: item.description,
+          flickr_image: item.flickr_images[0],
+          reserved: false,
+        }));
+        return {
+          ...state,
+          isLoading: false,
+          rockets: newArr,
+        };
+      })
+      .addCase(fetchedRockets.rejected, (state) => ({
+        ...state,
+        isLoading: false,
       }));
   },
 });
 
-export const { cancelReservation, makeReservation } = rocketsSlice.actions;
+export const { makeReservation, cancelReservation } = rocketsSlice.actions;
+
 export default rocketsSlice.reducer;
